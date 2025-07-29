ac5a50bd45fbe6495632d6105c8175e90ffae019
Stephan Haak <info@stephan-haak.com>
2025-07-29 10:40:37 +0200

    update styling for overiew route, userCard and inputForm

--- src/routes/overview/overview.tsx
+++ src/routes/overview/overview.tsx
@@ -1,10 +1,15 @@
+import "./overview.scss";
 import UserCard from "../../components/UserCard/UserCard";
 
 export default function Overview() {
   return (
     <>
       <h1>Ansicht - Seite</h1>
-      <UserCard />
+      <div className="overview">
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </div>
     </>
   );
 }
