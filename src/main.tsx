a3d1c62258b0ff9189562df961a8454f78d0a7f0
Stephan Haak <info@stephan-haak.com>
2025-09-08 08:25:42 +0200

    add ClickSpark

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import "../src/styles/index.scss";
 import App from "./App.tsx";
 import { UserProvider } from "./context/UserContext.tsx";
+import ClickSpark from "./components/ClickSpark/ClickSpark.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <ClickSpark
+      sparkColor="#daa520"
+      sparkSize={15}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+    >
+      {
+        <UserProvider>
+          <App />
+        </UserProvider>
+      }
+    </ClickSpark>
   </StrictMode>
 );
